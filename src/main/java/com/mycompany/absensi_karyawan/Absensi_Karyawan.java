f81a5915406229094ce0dd42613c930ef483d4cb
anjeli02 <ainjelitaikh@gmail.com>
2026-04-20 12:45:10 +0700

    first commit

---
+++ src/main/java/com/mycompany/absensi_karyawan/Absensi_Karyawan.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.absensi_karyawan;
+
+/**
+ *
+ * @author ain jelita ikhwati
+ */
+public class Absensi_Karyawan {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
